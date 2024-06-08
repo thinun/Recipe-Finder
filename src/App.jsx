fe3dcabe2e3e3238c10bf524d758245874fa0144
thinun <75312054+thinun@users.noreply.github.com>
2024-06-08 09:48:28 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Navbar from "./Componunts/Navbar/Navbar.jsx";
+import Home from "./Pages/Home/Home.jsx";
+import Favorite from "./Pages/Favorite/Favorite.jsx";
+import Details from "./Pages/Details/Details.jsx";
+import {Routes,Route} from "react-router-dom";
+
+function App() {
+
+
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/favorite"  element={<Favorite/>}/>
+        <Route path="/details-item/:id"  element={<Details/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/Favorite/Favorite.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Favorite = () => {
+    return (
+        <div>
+                Favorites
+        </div>
+    );
+};
+
+export default Favorite;
